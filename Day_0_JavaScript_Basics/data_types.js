689a3bba477868441c123e00393c0e69505a7fdd
Magilo-Ayuen <magiloayuen@gmail.com>
2026-09-01 13:51:45 +0300

    data types

---
+++ Day_0_JavaScript_Basics/data_types.js
@@ -0,0 +1,20 @@
+// Single line comment - Declaring variables
+
+const number = 12; //number type assingned to a number variable
+const floatNumber = 12.563;
+const isBoolean = true; // variable isBoolean is assigned isTrue
+const string = "This is a string";
+const empty = null; // Intensional left empty - undefined
+
+// OBJECTS - they are also data types
+
+const userData = {
+    firstName1: "Jack",
+    secondName2: "Jill",
+    age: 20
+};
+
+
+console.log(number,floatNumber,isBoolean,string);
+console.log(userData);
+ 
