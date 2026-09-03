6de984ede9a7e46434a02793c42f61aca1cae53d
Magilo-Ayuen <magiloayuen@gmail.com>
2026-09-03 05:24:28 +0300

    JavaScript Functions - introduction

---
+++ Day_2_JavaScript_Functions/intro_functions.js
@@ -0,0 +1,9 @@
+// Every standard function needs a declaration, parameters (inputs), and a return value (output).
+
+//declaration - functions are declared using function keyword
+//a and b are the parameters or inputs
+function add (a,b) {
+    return a + b;
+}
+//calling the add function with input parameters
+console.log(add(34,67));
