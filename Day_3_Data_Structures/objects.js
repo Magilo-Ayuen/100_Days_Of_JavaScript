7eb9374c86a288d68b6d3462cf1a6cdaafdb0f93
Magilo-Ayuen <magiloayuen@gmail.com>
2026-09-05 13:28:11 +0300

    Data Structures

---
+++ Day_3_Data_Structures/objects.js
@@ -0,0 +1,27 @@
+let person = {
+    name: "John Doe",
+    experience: ["Bid" , "kvda" , "depot"], //array
+    age: 29,
+    residence: "Nairobi",
+    email: "jd@gmail.com"
+};
+
+console.log(person.email);
+console.log(person.experience[2]);
+
+//ARRAY OF OBJECTS
+
+let kenyanCities = [
+    { riftValley: ["Nakuru", "Eldoret" , "Narok" ,"Lodwar"] ,
+        cities: false
+    },
+
+    {
+        western: ["Webuye","Kakamega","Busia"]
+    },
+
+    {
+        central:  ["Nyeri","Thika","Murang'a"]
+    }
+];
+
