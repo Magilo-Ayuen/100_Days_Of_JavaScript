c2bb4055831a83169765ee84b845b4a81de00da1
Magilo-Ayuen <magiloayuen@gmail.com>
2026-09-07 10:41:14 +0300

    Window object

---
+++ Day_4_DOM/window.js
@@ -0,0 +1,7 @@
+// Window object - represents the browser view while document object represents the webpage
+
+// All these are parts of window object - but we dont have to be writing window every now and then
+
+window.document //instead document.query* example is best
+window.console.log // ''
+window.alert // ''
