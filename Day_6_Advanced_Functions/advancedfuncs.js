9f782533265a53f76d0a4ae1119b4d35c3d6adbe
Magilo-Ayuen <magiloayuen@gmail.com>
2026-09-11 04:38:10 +0300

    Advanced Js Functions - .js file

--- Day_6_Advanced_Functions/advancedfuncs.js
+++ Day_6_Advanced_Functions/advancedfuncs.js
@@ -20,4 +20,55 @@ const object1 = {
 
 console.log(object1.fun()); // or just object.fun();
 
+//Callback in js
 
+function display(param) {
+    console.log(param);
+}
+display(2026);
+
+function run (param) {
+    param();
+}
+
+run (function (){
+    console.log('hello tena!')
+});
+
+//This is called as passing a function into another function 
+
+// THe function that we are passing in is known as a callback function
+
+
+//----------------PRACTICAL EXAMPLES { Using Functions as Values } --------------------
+
+/*        
+        1. setTimeout() 
+             - Allows us to run a function in the future.
+             - Is a built-in function. 
+             - It usually take two parameters {1.func we want to run in the future , 2.how long before running this func in millisec}
+
+*/
+
+setTimeout(function(){
+    console.log('timeout');
+}, 3000);
+
+//setTimeout is known as Asynchronous code - won't wait for a line to finish before going to the next line
+// Sychronous code on the other hand- will wait for one line to finish before going to the next line
+
+
+/*        
+        1. setInterval() 
+             - Allows us to run a function in the future.
+             - Is a built-in function. 
+             - It usually take two parameters {1.func we want to run in the future , 2.how long before running this func in millisec}
+
+
+             -It will keep running a function in the future
+
+*/
+
+setInterval(function() {
+    console.log('interval');
+}, 3000);
